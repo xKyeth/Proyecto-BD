f1cb89c394bff6e9864017238def2a0318f0805e
pecorte <pecorte@yahoo.es>
2019-04-10 13:27:51 +0200

    Update RequisitosProyectoBD.sql

--- RequisitosProyectoBD.sql
+++ RequisitosProyectoBD.sql
@@ -2,6 +2,25 @@
 --Requisito Nº2
 --Requisito Nº3
 --Requisito Nº4
+create or replace procedure BurcarHotel(hotel varchar2)
+is
+    id number;
+    nombre varchar2(50);
+    Cursor hoteles is Select h.idHotel, h.nomHotel
+        from hotel h, ciudad c
+        where c.nomCiudad=hotel and c.idCiudad=h.idCiudad;    
+begin
+open hoteles;
+    FETCH Hoteles INTO id, nombre;
+    if hoteles%NOTFOUND then
+        DBMS_OUTPUT.PUT_LINE('No hay ningun hotel');
+    end if;
+close hoteles;
+    for reg in hoteles loop
+        DBMS_output.put_line('Nombre: '|| reg.nomHotel || ', ID: ' || reg.idHotel);
+    end loop;    
+end BurcarHotel;
+/
 --Requisito Nº5
 --Requisito Nº6
 --Requisito Nº7
