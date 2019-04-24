cfb17f2fa81cb851ccf762a6ac8485b5415fd79e
pecorte <pecorte@yahoo.es>
2019-04-24 19:12:54 +0200

    Update RequisitosProyectoBD.sql

--- RequisitosProyectoBD.sql
+++ RequisitosProyectoBD.sql
@@ -1,5 +1,40 @@
---Requisito NÂº1
---Requisito NÂº2
+--Requisito Nº1
+
+/*Nombre componente: Enrique Albors Perilli
+Autor: Enrique Albors Perilli	Fecha:10/04/2019
+Descripción:  Este procedimiento , buscará  habitaciones según tipo y nº camas ,mostrar habitaciones disponibles por hotel, se podrá elegir un hotel para mostrar datos de este y las habitaciones.*/
+
+
+
+create or replace procedure buscaHabitacion(cod char , cod2 number)is
+
+    cursor Y (cod char , cod2 number) IS
+
+        select  h.nomhotel as codigo_hotel , ha.cantcamas as numero_camas  
+        from hotel h , habitacion ha 
+        where  h.IDHOTEL = ha.IDHOTEL and ha.cantcamas=cod and h.IDHOTEL=cod2 ;
+
+    registro Y%rowtype;
+
+begin
+open Y (cod ,cod2);
+
+    if (Y%notfound)then
+            DBMS_OUTPUT.PUT_LINE('no existe los datos que usted pide');
+        else 
+         loop
+            fetch Y into registro;
+                exit when Y%notfound;
+                DBMS_OUTPUT.PUT_LINE(registro.codigo_hotel ||' tiene  '|| registro.numero_camas);
+         end loop;
+    end if;
+close Y;
+end;
+/
+
+exec buscaHabitacion ( '1' ,'11111' );
+
+--Requisito Nº2
 /*Nombre componente: comprHabitacion
 Autor: Pedro Cortes	Fecha: 22/04/19
 Descripción: Funcion que comprueba las reservas y devuelve si una habitaciÃ³n se debe ocupar*/
@@ -48,10 +83,9 @@ BEGIN
 END ocuparHabitaciones;
 /
 
---Requisito NÂº3
+--Requisito Nº3
 
-
-
+    
 /*Funcion que recibe por parametro el ID de prepago y verifica que existe en la base de datos.Si existe devuelve true, en caso de que no
 devolvera un false.*/
 
@@ -118,9 +152,49 @@ rellenarprephuesped(idH,idP);
 END;
 /
 
---Requisito NÂº4
+--Requisito Nº4
 
---Requisito NÂº5
+
+ /*Nombre componente: Enrique Albors Perilli
+Autor: Enrique Albors Perilli	Fecha: 11/04/2019
+Descripción: Este procedimiento permitira´ que se cree un usuario automáticamente pasando un email y su passw
+ademas para controlar que la longitud de la contraseña no excede a lo que que queremos , se controlará la longitud de 
+la contraseña con una función*/
+
+create or replace procedure creaUsuario2 (usuario char , passwd char) is
+ 
+    cursor Y (usuario char , passwd char) is 
+    
+        select usremail  , usrpwd from usuario ; 
+       
+        registro Y%rowtype;
+       
+begin
+        open Y (usuario , passwd);
+            fetch Y into registro;
+                if (validaContra(passwd)=true)then
+                 INSERT INTO usuario VALUES (usuario,passwd);
+                end if;    
+end;
+/
+
+ exec creaUsuario2 ('3' , '3444558');
+ select usremail  , usrpwd from usuario ;
+
+--funcion validacion contra
+create or replace function validaContra (contra varchar2)
+return boolean is
+    begin 
+        if (LENGTH(contra)>6)then
+            DBMS_OUTPUT.PUT_LINE('la longitud de su contraseña debe ser menor');RETURN false;
+        else 
+             DBMS_OUTPUT.PUT_LINE('la longitud de su  contraseña es correcta');return true;
+        end if;
+end validaContra;
+/
+
+
+--Requisito Nº5
 /*Procedimiento que pasado un nombre de ciudad comprueba si existen hoteles en dicha ciudad*/
 create or replace procedure BuscarHotel(ciudad varchar2)
 is
@@ -138,7 +212,7 @@ open hoteles;
         DBMS_OUTPUT.PUT_LINE('No hay ningun hotel');
     end if;
 close hoteles;
-    --Aqui muestro los registros de los del cursor, sino hubiese ninguno darÃ­a igual, ya que no se mostrarÃ­a niguno y
+    --Aqui muestro los registros de los del cursor, sino hubiese ninguno daría igual, ya que no se mostraría niguno y
     --y se activa el if anterior
     for reg in hoteles loop
         DBMS_output.put_line('Nombre: '|| reg.nomHotel || ', ID: ' || reg.idHotel);
@@ -146,7 +220,7 @@ close hoteles;
 end BuscarHotel;
 /
 
---Requisito NÂº6
+--Requisito Nº6
 
 /*Procedimiento al que se le pasa un ID Huesped por parametro, se verifica que exista el ID pasado, si es asi, se imprimira una ficha
 con todos los datos del Huesped.*/
@@ -199,42 +273,118 @@ END;
 /
 
 
---Requisito NÂº7
---Requisito NÂº8
-/*Create or replace procedure pagosPais(pais varchar2)
-is
-    Cursor clientes is Select 
+--Requisito Nº7
+
+/*Nombre componente: Enrique Albors Perilli
+Autor: Enrique Albors Perilli	Fecha: 17/04/2019
+Descripcion : Este procedimiento devolverá todos los datos relevantes de un hotel a raiz del codigo del hotel.
+*/
+
+
+ create or replace procedure fichaHoteles (cod number) is
+ 
+     cursor Y (cod number) is 
+     select h.nomhotel as nombre , c.nomciudad as ciudad , p.nompais as pais from hotel h , ciudad c , pais p where h.idhotel=cod and h.idciudad=c.idciudad and c.idpais = p.idpais  ;   
+     registro Y%rowtype;
+     
+     cursor X (cod number) is
+     select count (ha.cantcamas) as numero from habitacion ha , hotel h  where h.idhotel=cod and h.idhotel=ha.idhotel; 
+     habita X%rowtype;
+     
+     cursor simple (cod number) is 
+     select count (ha.cantcamas) as numero from habitacion ha , hotel h  where h.idhotel=cod and h.idhotel=ha.idhotel and ha.idtipohab='SNG'; 
+     habita1 simple%rowtype;
+     
+     cursor doble (cod number) is 
+     select count (ha.cantcamas) as numero from habitacion ha , hotel h  where h.idhotel=cod and h.idhotel=ha.idhotel and ha.idtipohab='DBL'; 
+     habita2 doble%rowtype;
+     
+     cursor triple (cod number) is 
+     select count (ha.cantcamas) as numero from habitacion ha , hotel h  where h.idhotel=cod and h.idhotel=ha.idhotel and ha.idtipohab='BRK'; 
+     habita3 triple%rowtype;
+     
+     cursor lujo (cod number) is 
+     select count (ha.cantcamas) as numero from habitacion ha , hotel h  where h.idhotel=cod and h.idhotel=ha.idhotel and ha.idtipohab='LUX'; 
+     habita4 lujo%rowtype;
+       
 begin
-end pagosPais;*/
---Requisito NÂº9
+    open Y (cod) ;
+    fetch Y into registro;
+            DBMS_OUTPUT.PUT_LINE('************************************************************');
+            DBMS_OUTPUT.PUT_LINE( 'Hotel :  '|| registro.nombre ||'   Pais:'  || registro.pais ||'  Ciudad  :'|| registro.ciudad );
+    close Y;
 
-create or replace FUNCTION comprHuesped (idHues NUMBER) RETURN BOOLEAN IS
+    open X (cod);
+    fetch X into habita;
+            DBMS_OUTPUT.PUT_LINE( 'la cantidad total de camas de este hotel es  '||habita.numero );
+    close X;
 
-CURSOR curs IS
-    SELECT idhuesped FROM huesped WHERE idhuesped=idHues; 
-registro curs%ROWTYPE;
+    open simple (cod);
+        fetch simple into habita1;
+            DBMS_OUTPUT.PUT_LINE( 'la cantidad total de camas simples  es  '||habita1.numero );
+    close simple;
 
-BEGIN
+    open doble (cod);
+        fetch doble into habita2;
+            DBMS_OUTPUT.PUT_LINE( 'la cantidad total de camas simples  es  '||habita2.numero );
+    close doble;
 
-OPEN curs;
+    open triple (cod);
+        fetch triple into habita3;
+            DBMS_OUTPUT.PUT_LINE( 'la cantidad total de camas simples  es  '||habita3.numero );
+    close triple;
 
-    FETCH curs INTO registro;
+    open lujo (cod);
+        fetch lujo into habita4;
+            DBMS_OUTPUT.PUT_LINE( 'la cantidad total de camas simples  es  '||habita4.numero );
+    close lujo;
 
-    IF idHues IN (registro.idhuesped) THEN
-        DBMS_OUTPUT.PUT_LINE('El huesped existe en la base de datos');
-        RETURN TRUE;
-    ELSE
-        DBMS_OUTPUT.PUT_LINE('El huesped no existe en la base de datos');
-        RETURN false;
-    END IF;
+end;
+/
 
-CLOSE curs;
-END;
+exec fichahoteles ('14325');
+
+--Requisito Nº8
+Create or replace procedure pagosPais(idHues number, pais varchar2)
+is
+
+begin
+end pagosPais;
+/
+--Requisito Nº9
 
 
 
---Requisito NÂº10
---Requisito NÂº11
+--Requisito Nº10
+
+/*Nombre componente: Enrique Albors Perilli
+Autor: Enrique Albors Perilli	Fecha: 11/04/2019
+Descripcion : Procedimiento que a raiz de un codigo de pago , informará si el metodo de pago es aceptado o no .
+*/
+
+create or replace procedure pagoCorrecto (codigo char ) is
+
+Cursor Y (codigo char) is
+    select empresa from metodo_de_pago where codigo_metodo=codigo;
+
+registro Y%rowtype;
+
+begin 
+
+    open Y (codigo );
+       if (codigo >=7)then
+          DBMS_OUTPUT.PUT_LINE('el metodo de pago no existe');
+       else
+          DBMS_OUTPUT.PUT_LINE('El metodo de pago es correcto');
+       end if;
+close Y;
+end;
+/
+
+
+EXEC PAGOCORRECTO (8);
+
+--Requisito Nº11
 Create or replace procedure pagosAnuales(idHues number, ano number)
 is
     Cursor Total is Select (h.nomhuesped || ' ' || h.apepaterno || ' ' || h.apematerno ) as Nombre, h.idhuesped, To_Char(NVL(SUM(pr.total), '0'), '999g999g990d99l' ) as totalpag
@@ -255,8 +405,7 @@ begin
     end loop;
 end pagosAnuales;
 /
---Requisito NÂº12
-
+--Requisito Nº12
 
 CREATE OR REPLACE FUNCTION aplicarDescuento (idCup VARCHAR2, precio NUMBER) RETURN NUMBER IS
 
@@ -279,7 +428,7 @@ OPEN CURS;
     v_precioConDesc := precio/registro.descuento;        
     
     DBMS_OUTPUT.PUT_LINE('El precio original es : ' || precio || ' al que se le ha aplicado un descuento de : ' || registro.descuento || 
-    ' del cupÃ³n : ' || idCup || ' ,el precio con el descuento aplicado es : ' || ROUND(v_preciocondesc,2));
+    ' del cupón : ' || idCup || ' ,el precio con el descuento aplicado es : ' || ROUND(v_preciocondesc,2));
     
     END IF;
     
@@ -290,8 +439,8 @@ END;
 /
 
 
---Requisito NÂº13
---Requisito NÂº14
+--Requisito Nº13
+--Requisito Nº14
 Create or replace procedure imprReserva(idR number)
 is
     Cursor reserva is Select r.* ,rg.descrRegimen, h.nomHotel  
@@ -310,8 +459,8 @@ Begin
     DBMS_OUTPUT.PUT_LINE('ID de la Reserva : ' || registro.idReserva);
     DBMS_OUTPUT.PUT_LINE('Email del usuario : ' || registro.usremail);
     DBMS_OUTPUT.PUT_LINE('Hotel : ' || registro.nomHotel);
-    DBMS_OUTPUT.PUT_LINE('  NÂº Habitacion : ' || registro.numHabitacion);
-    DBMS_OUTPUT.PUT_LINE('  NÂº Huespedes : ' || registro.canthuesp);
+    DBMS_OUTPUT.PUT_LINE('  Nº Habitacion : ' || registro.numHabitacion);
+    DBMS_OUTPUT.PUT_LINE('  Nº Huespedes : ' || registro.canthuesp);
     DBMS_OUTPUT.PUT_LINE('  Tipo de reserva : ' || registro.descrRegimen);
     DBMS_OUTPUT.PUT_LINE('Fecha de la reserva : ' || registro.fchReserva);
     DBMS_OUTPUT.PUT_LINE('  Inicio de la reserva : ' || registro.fchDesde);
@@ -322,4 +471,4 @@ Begin
     end if;
 end imprReserva;
 /
---Requisito NÂº15
+--Requisito Nº15
