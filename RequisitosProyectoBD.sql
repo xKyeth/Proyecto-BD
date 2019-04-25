70099670e23be4757c94b8d8bcc02a4e4f38eca9
xKyeth <imanol.garcia.prieto.alu@iesjulioverne.es>
2019-04-25 20:55:12 +0200

    Update RequisitosProyectoBD.sql

--- RequisitosProyectoBD.sql
+++ RequisitosProyectoBD.sql
@@ -354,8 +354,8 @@ end reservasPais;
 /
 --Requisito Nº9
 
-/*Nombre componente: Enrique Albors Perilli
-Autor: Enrique Imanol Garcia Fecha: 25/04/2019
+/*Nombre componente: comprHuesped
+Autor:Imanol Garcia Fecha: 25/04/2019
 Descripcion : Funcion que recibe el idHuesped por parametro que comprueba si existe en la base de datos, si existe devuelve true si no
 devuelve false*/
 
@@ -550,6 +550,7 @@ INSERT INTO AUDITORIA VALUES (gd);
 
 END;
 /
+
 
 
 
