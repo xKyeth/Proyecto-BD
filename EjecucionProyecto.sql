ef6e502e33e7f51d80b27d4a5b033298f11fb66e
pecorte <pecorte@yahoo.es>
2019-04-25 11:13:04 +0200

    Update EjecucionProyecto.sql

---
+++ EjecucionProyecto.sql
@@ -0,0 +1,107 @@
+SET SERVEROUTPUT ON;
+--Requisito Nº1
+Declare
+    
+Begin
+    
+End;
+/
+--Requisito Nº2
+Declare
+    
+Begin
+    ocuparHabitaciones;
+End;
+/
+--Requisito Nº3
+Declare
+    
+Begin
+    
+End;
+/
+--Requisito Nº4
+Declare
+    
+Begin
+    
+End;
+/
+--Requisito Nº5
+Declare
+    Nombre varchar2(20):='&Nombre';
+Begin
+    BuscarHotel(Nombre);
+End;
+/
+--Requisito Nº6
+Declare
+    
+Begin
+    
+End;
+/
+--Requisito Nº7
+Declare
+    
+Begin
+    
+End;
+/
+--Requisito Nº8
+Declare
+    Nombre varchar2(20):='&Nombre';
+Begin
+    reservasPais(Nombre);
+End;
+/
+--Requisito Nº9
+Declare
+    
+Begin
+    
+End;
+/
+--Requisito Nº10
+Declare
+    
+Begin
+    
+End;
+/
+--Requisito Nº11
+Declare
+    ano number:='&AÑO';
+    id number:='&ID_Cliente';
+Begin
+    pagosAnuales(id, ano);
+End;
+/
+--Requisito Nº12
+Declare
+    
+Begin
+    
+End;
+/
+--Requisito Nº13
+Declare
+    
+Begin
+    
+End;
+/
+--Requisito Nº14
+Declare
+    id number:=('&ID_Reserva');
+Begin
+    imprReserva(id);
+End;
+/
+--Requisito Nº15
+Declare
+    
+Begin
+    
+End;
+/
